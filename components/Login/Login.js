65227a0a46acf2c5149e435fc76285d751536e74
Perry Janssen <perry@burst-digital.com>
2019-05-09 15:19:06 +0200

    feat(login): login styling

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -1,60 +1,100 @@
 import React from 'react';
-import {Image, ScrollView, StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+import {Image, StyleSheet, View, Text, TouchableOpacity} from 'react-native';
 import SpotifyAuth from '../../services/Spotify/SpotifyAuth';
+import {LinearGradient, Font} from 'expo';
 
 export default class Login extends React.Component {
+
+  state = {
+    fontLoaded: false,
+  };
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'ProximaNova': require('../../assets/fonts/ProximaNovaBold.otf'),
+    });
+
+    this.setState({ fontLoaded: true });
+  }
 
   render() {
     // AsyncStorage.clear();
     return (
-      <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+      <LinearGradient
+        colors={['#e5127d', '#e9316c', '#fff500']}
+        style={styles.gradient}
+        start={{x: 0, y: 0}}
+        end={{ x: 1, y: 1 }}
+        locations={[0.1, 0.4, 0.9]}
+      >
+        <View style={styles.container}>
           <View style={styles.logoContainer}>
             <Image
-              source={require('../../assets/images/splash.png')}
+              source={require('../../assets/images/logo-white.png')}
               style={styles.logo}
             />
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => SpotifyAuth.login(this.props.context)}
-            >
-              <Text style={styles.buttonText}>
-                LOGIN WITH SPOTIFY
-              </Text>
-            </TouchableOpacity>
+            {
+              this.state.fontLoaded ? (
+                <Text style={styles.subTitle}>
+                  BUST YOUR BUBBLE
+                </Text>
+              ) : null
+            }
           </View>
-        </ScrollView>
-      </View>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => SpotifyAuth.login(this.props.context)}
+          >
+          {
+            this.state.fontLoaded ? (
+              <Text style={styles.buttonText}>
+                log in with Spotify
+              </Text>
+            ) : null
+          }
+          </TouchableOpacity>
+        </View>
+      </LinearGradient>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  gradient: {
     flex: 1,
-    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  contentContainer: {
-    paddingTop: 30,
+  container: {
+    width: '70%',
   },
   logoContainer: {
     alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
+    marginBottom: 100,
   },
   logo: {
-    width: 200,
-    height: 100,
+    width: '100%',
+    height: 50,
     resizeMode: 'contain',
+    marginBottom: 10,
+  },
+  subTitle: {
+    fontFamily: 'ProximaNova',
+    fontSize: 27,
+    color: '#fff',
   },
   button: {
-    backgroundColor: 'rgba(255, 255, 255, 0.3)',
-    paddingVertical: 15,
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
     width: '100%',
+    borderRadius: 50,
+    height: 70,
+    justifyContent: 'center',
   },
   buttonText: {
-    color: '#000',
+    color: '#fff',
     textAlign: 'center',
     fontWeight: '700',
+    fontFamily: 'ProximaNova',
+    fontSize: 24,
   }
 });
